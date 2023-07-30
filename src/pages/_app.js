1afd1cdd4a725f5722011cdf57e8cdb6f7ae4828
AmanatUMahmud <amanatmahmud@gmail.com>
2023-07-30 19:30:22 +0600

    Next auth set up done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
