660462fb15ee4ba60f9d29b6baab1fc32fe6e528
AmanatUMahmud <amanatmahmud@gmail.com>
2023-07-30 11:06:25 +0600

    Product details page created

---
+++ src/pages/product/[productId].jsx
@@ -0,0 +1,14 @@
+import { Router, useRouter } from 'next/router';
+import React from 'react';
+
+const ProductDetails = () => {
+    const router = useRouter();
+    const {productId} = router.query;
+    return (
+        <div>
+            {productId}
+        </div>
+    );
+};
+
+export default ProductDetails;
