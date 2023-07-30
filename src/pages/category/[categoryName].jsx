c955674b4c74c44b8a126efbaceb8086ed11a9c9
AmanatUMahmud <amanatmahmud@gmail.com>
2023-07-30 18:45:32 +0600

    Categories done

--- src/pages/category/[categoryName].jsx
+++ src/pages/category/[categoryName].jsx
@@ -1,9 +1,14 @@
+import FeaturedProduct from "@/components/FeaturedProduct";
 import RootLayout from "@/components/Layouts/RootLayout";
 import React from "react";
 
 const CategoryPage = ({allProducts}) => {
-    console.log(allProducts);
-  return <div></div>;
+  return <section className="m-6">
+  <h1 className="text-3xl font-bold">{`Category/${(allProducts[0].category).toUpperCase()}`}</h1>
+  <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6'>
+  {allProducts.map(product=><FeaturedProduct key={product._id} product={product}></FeaturedProduct>)}
+</div>
+</section>;
 };
 
 export default CategoryPage;
